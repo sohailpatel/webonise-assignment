4750b2631fce49b13f1abcde740fed4132a641e4
sohailpatel <sohailpatelt20@gmail.com>
2016-02-01 11:29:00 -0800

    first file created

---
+++ assingment-1/example1.js
@@ -0,0 +1,7 @@
+
+document.writeln("Name is " + navigator.appName + ". Code name is " + navigator.appCodeName);
+
+document.writeln("Height of screen is "+screen.availHeight +"\n");
+document.writeln(" Width of the screen is "+ screen.availWidth);
+
+document.writeln(" Last modified at "+  document.lastModified );
