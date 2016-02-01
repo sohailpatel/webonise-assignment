3bc6c7d3d664873a7ad97f9b92fb4341c02c06a2
sohailpatel <sohailpatelt20@gmail.com>
2016-02-01 11:35:11 -0800

    file 7 created

---
+++ assingment-1/example7.js
@@ -0,0 +1,16 @@
+var David=80; 
+var Vinoth=77; 
+var Divya=88;
+var Ishitha=95; 
+var Thomas=68;
+var avg=(David+Vinoth+Divya+Ishitha+Thomas)/5;
+if(avg<60)
+	document.write("F");
+else if(avg<70)
+	document.write("D");
+else if(avg<80)
+	document.write("C");
+else if(avg<90)
+	document.write("B");
+else if(avg<100)
+	document.write("A");
