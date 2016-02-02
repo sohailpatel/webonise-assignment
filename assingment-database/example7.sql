d545a077dafbc5fc8bfe770f269400cfd9ec3c98
sohailpatel <sohailpatelt20@gmail.com>
2016-02-02 10:29:53 -0800

    added question to example7.sql

--- assingment-database/example7.sql
+++ assingment-database/example7.sql
@@ -1,1 +1,5 @@
+/*
+If a manager manages more than one department, he or she controls the sum of all the budgets for those departments. Find the manager_ids and names  of managers who control more than $5 million.
+*/
+
 select id,empname from emp where  id in(select manid from dept  group by manid having sum(budget)>5000);
