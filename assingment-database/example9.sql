b80f7143be4fd4bb843b2e9fb6f00872f23fa38f
sohailpatel <sohailpatelt20@gmail.com>
2016-02-02 10:31:50 -0800

    added question to example9.sql

--- assingment-database/example9.sql
+++ assingment-database/example9.sql
@@ -1,1 +1,4 @@
+/*
+Find the names of managers who manage only departments with budgets larger than $1 million, but at least one department with budget less than $5 million. 
+*/
 select dept.manid,emp.empname from dept,emp where emp.id=dept.manid and dept.budget>1000000 and dept.budget<5000000 group by dept.manid,emp.empname;
