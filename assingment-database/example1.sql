c4b8ca90ce87890e4bc2afd143b72c8c858fff1b
sohailpatel <sohailpatelt20@gmail.com>
2016-02-02 10:25:45 -0800

    added question to example1.sql

--- assingment-database/example1.sql
+++ assingment-database/example1.sql
@@ -1,1 +1,5 @@
+/*
+Print the names and ages of each employee who works in both the Hardware department and the Software department. 
+*/
+
 select empname,age from emp where id in (SELECT a.empid from ( select empid from works where deptid=1)a,( select empid from works where deptid=2)b  where a.empid=b.empid);
