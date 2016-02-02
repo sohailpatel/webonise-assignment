419f97642f5776b528c8ef475c0a7c5024936f13
sohailpatel <sohailpatelt20@gmail.com>
2016-02-02 10:28:59 -0800

    added question to example6.sql

--- assingment-database/example6.sql
+++ assingment-database/example6.sql
@@ -1,1 +1,4 @@
+/*
+Find the names of managers who manage the departments with the largest budgets. 
+*/
 select empname from emp where id in (select manid from dept where budget = (select max(budget) from dept));
