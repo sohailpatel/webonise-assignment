f9987f4ae727368b765ec7d3bf86b6e4b2f2785d
sohailpatel <sohailpatelt20@gmail.com>
2016-02-02 10:30:43 -0800

    added question to example8.sql

--- assingment-database/example8.sql
+++ assingment-database/example8.sql
@@ -1,1 +1,10 @@
+/*
+Find the manager_ids and names of managers who control the largest amounts. 
+*/
+
 select id,empname from emp where id in( select top 1 manid from dept group by manid having max(budget));
+
+/*all entries 
+
+select dept.manid,emp.empname from dept,emp where emp.id=dept.manid group by dept.manid,emp.empname having max(dept.budget);
+*/
