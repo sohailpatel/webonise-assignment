b32546680fc063e96b34ec6f17aa9318291265ad
sohailpatel <sohailpatelt20@gmail.com>
2016-02-02 10:26:56 -0800

    added question to example4.sql

--- assingment-database/example4.sql
+++ assingment-database/example4.sql
@@ -1,1 +1,6 @@
+/*
+ Print the name of each employee whose salary exceeds the budget of all of the departments that he or she works in. 
+*/
+
+
 select empname from emp where salary>(select sum(budget) from dept where id in(select deptid from works where works.empid=emp.id));
