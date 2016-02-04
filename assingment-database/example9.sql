2bcc24963161c08bf0af6923a111863587a10616
sohailpatel <sohailpatelt20@gmail.com>
2016-02-04 09:43:53 -0800

    Update example9.sql

--- assingment-database/example9.sql
+++ assingment-database/example9.sql
@@ -1,4 +1,4 @@
 /*
 Find the names of managers who manage only departments with budgets larger than $1 million, but at least one department with budget less than $5 million. 
 */
-select dept.manid,emp.empname from dept,emp where emp.id=dept.manid and dept.budget>10000 and (select top 1 manid from dept where budget>50000  group by manid having  count(budget)=1 ) group by dept.manid,emp.empname  ;
+select dept.manid,emp.empname from dept,emp where emp.id=dept.manid and dept.budget>10000 and (select top 1 manid from dept where budget>50000  group by manid having  count(budget)>=1 ) group by dept.manid,emp.empname  ;
